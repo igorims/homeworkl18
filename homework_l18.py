c8ca525c65732b43ba4abd4ee43f5c61c2994bd1
Igor <fatalhope@gmail.com>
2021-03-31 13:21:10 +0300

    Added 2nd solution and colored print

--- homework_l18.py
+++ homework_l18.py
@@ -6,6 +6,20 @@
 """
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKGREEN = '\033[92m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    UNDERLINE = '\033[4m'
+    LIGHTGRAY = "\033[37m"
+
+
+# Комментарий. По заданию не очень понятно, нужно ли складывать и сравнивать целые и дробные числа ОДНОГО числа,
+# указанного при определении инстанса (решение 1) или просто производить операции над двумя инстансами класса
+# (решение 2), поэтому сделал оба решения.
+
+# Решение 1
 class FractionNum:
     def __init__(self, number):
         self.number = str(number)
@@ -13,6 +27,8 @@ class FractionNum:
         integer, decimal = self.number.split('.')
         self.integer = int(integer)
         self.decimal = int(decimal)
+
+        print(f'{bcolors.OKGREEN}Number: {self.number}{bcolors.ENDC}')
 
     def __add__(self, other):
         print(f'{self.integer} + {self.decimal} = {self.integer + self.decimal}')
@@ -24,6 +40,8 @@ class FractionNum:
         print(f'{self.integer} * {self.decimal} = {self.integer * self.decimal}')
 
     def __eq__(self, other):
+        if {self.integer == self.decimal} == False:
+            return f'{self.integer == self.decimal}'
         print(f'{self.integer} == {self.decimal} is {self.integer == self.decimal}')
 
     def __ne__(self, other):
@@ -42,7 +60,61 @@ class FractionNum:
         print(f'{self.integer} >= {self.decimal} is {self.integer >= self.decimal}')
 
 
+# Решение 2. В объект вводятся 2 числа. По дробной части проходится проверка на положительное/отрицательное число, а
+# также на ввод в виде строки (чтобы, например, написать 003), строка также проверяется на ввод отрицательного значения.
+# Далее из обоих чисел делается одно число number и производятся необходимые операции
+
+import re
+class FractionNum1:
+    counter = 1
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+        if isinstance(num2, str):
+            if self.num2.startswith('-'):
+                num2 = num2[1:]
+            self.number = str(num1) + "." + num2
+
+        else:
+            self.number = str(num1) + "." + str(abs(num2))
+        self.number = float(self.number)
+
+        print(f'{bcolors.OKGREEN}Number {self.counter}: {self.number}{bcolors.ENDC}')
+
+        FractionNum1.counter += 1
+
+    def __add__(self, other):
+        print(f'{self.number} + {other.number} = {self.number + other.number}')
+
+    def __sub__(self, other):
+        print(f'{self.number} - {other.number} = {(self.number - other.number):.2}')
+
+    def __mul__(self, other):
+        print(f'{self.number} * {other.number} = {(self.number * other.number):.4}')
+
+    def __eq__(self, other):
+        print(f'{self.number} == {other.number} is {self.number == other.number}')
+
+    def __ne__(self, other):
+        print(f'{self.number} != {other.number} is {self.number != other.number}')
+
+    def __lt__(self, other):
+        print(f'{self.number} < {other.number} is {self.number < other.number}')
+
+    def __le__(self, other):
+        print(f'{self.number} <= {other.number} is {self.number <= other.number}')
+
+    def __gt__(self, other):
+        print(f'{self.number} > {other.number} is {self.number > other.number}')
+
+    def __ge__(self, other):
+        print(f'{self.number} >= {other.number} is {self.number >= other.number}')
+
+
 def main():
+    print(f'{bcolors.HEADER}Решение 1{bcolors.ENDC}')
     testNum = FractionNum(12.5)
     testNum + testNum
     testNum - testNum
@@ -53,6 +125,20 @@ def main():
     testNum <= testNum
     testNum > testNum
     testNum >= testNum
+
+    print()
+    print(f'{bcolors.HEADER}Решение 2{bcolors.ENDC}')
+    testNum1 = FractionNum1(10, -2000)
+    testNum2 = FractionNum1(5, '-003')
+    testNum1 + testNum2
+    testNum1 - testNum2
+    testNum1 * testNum2
+    testNum1 == testNum2
+    testNum1 != testNum2
+    testNum1 < testNum2
+    testNum1 <= testNum2
+    testNum1 > testNum2
+    testNum1 >= testNum2
 
 
 if __name__ == '__main__':
